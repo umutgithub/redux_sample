2c10aac68dcfbbfa620d67ede509a78ee9528a2b
umut <umut.sevil@vertexinc.com>
2018-03-30 22:57:54 -0400

    store folder created and reducer.js created

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+/**
+ * Created by usevil on 3/30/18.
+ */
+
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state = initialState, action ) => {
+    return state;
+};
+
+export default reducer;
