b42a5c5b40c9337f52a879fad6f8f9d06a15c82d
umut <umut.sevil@vertexinc.com>
2018-04-23 10:36:16 -0400

    add redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 
 import './index.css';
@@ -28,9 +28,11 @@ const logger = store => {
          }
      }
 };
+//variable to enable redux dev tool. then it wraps applyMiddleware
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //pass the reducer, middleware(2nd argument)
-const store = createStore(rootReducer , applyMiddleware(logger));
+const store = createStore(rootReducer , composeEnhancers(applyMiddleware(logger)));
 
 //provide needed  to pass store to components
 ReactDOM.render(<Provider store = {store}><App /></Provider> , document.getElementById('root'));
