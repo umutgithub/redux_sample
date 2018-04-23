5df0abc3c217a7e24562389d3d93976019e9c64c
umut <umut.sevil@vertexinc.com>
2018-04-23 10:18:11 -0400

    p269 adding middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 
 import './index.css';
@@ -15,8 +15,22 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-//pass the reducer
-const store = createStore(rootReducer);
+
+//middleware
+const logger = store => {
+     return next => {
+         return action => {   //action is the action we dispatch
+            console.log('[middleware dispathing]' , action);
+            const result = next(action);
+            console.log('[result ]' , result);
+            console.log('[middleware nextstate]', store.getState());
+            return result;
+         }
+     }
+};
+
+//pass the reducer, middleware(2nd argument)
+const store = createStore(rootReducer , applyMiddleware(logger));
 
 //provide needed  to pass store to components
 ReactDOM.render(<Provider store = {store}><App /></Provider> , document.getElementById('root'));
